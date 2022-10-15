e64b58f065583ab4830e2f079a2fbbb21acb720f
vito mohagheghian <vito.mohagheghian@gmail.com>
2022-10-15 22:58:50 +0330

    fix save button, navbar tablet view display none, and refactor single page classNames nad components design

--- src/components/singleEvent/comments.tsx
+++ src/components/singleEvent/comments.tsx
@@ -1,77 +1,25 @@
 import Comment from "./comment";
 
 import CommentIcon from "../../assets/icons/comment.svg";
-import DoubleTIcon from "../../assets/icons/double-t.svg";
-import UnderlineColorsIcon from "../../assets/icons/underline-colors.svg";
-import OrderedListIcon from "../../assets/icons/ordered-list.svg";
-import UnOrderedListIcon from "../../assets/icons/unordered-list.svg";
-import JustifyTextIcon from "../../assets/icons/justify-text.svg";
-import RightTextIcon from "../../assets/icons/right-text.svg";
-import CenterTextIcon from "../../assets/icons/right-text.svg";
-import UnderlineIcon from "../../assets/icons/underline.svg";
-import ItalicIcon from "../../assets/icons/italic.svg";
-import BoldIcon from "../../assets/icons/bold.svg";
 
 import "../../styles/comments.scss";
+import CreateComment from "./createComment";
 
 function Comments() {
   return (
-    <section className="event__comments">
-      <div className="comments__header">
+    <section className="event__comments comments">
+      <div className="comments__header header">
         <img src={CommentIcon} alt="" />
         <h2>نظرات شرکت کنندگان</h2>
-        <span className="header__comments__count">
+        <span className="header__count">
           (<span>15</span> نظر)
         </span>
       </div>
-      <div className="comments__content">
-        <div className="new-comment">
-          <div className="new-comment__header">
-            <select name="" id="">
-              <option value="paragraph">پاراگراف</option>
-              <option value="heading1">عنوان اول</option>
-              <option value="heading2">عنوان دوم</option>
-              <option value="heading3">عنوان سوم</option>
-            </select>
-            <button className="first-button">
-              <img src={DoubleTIcon} alt="" />
-            </button>
-            <button>
-              <img src={UnderlineColorsIcon} alt="" />
-            </button>
-            <button>
-              <img src={OrderedListIcon} alt="" />
-            </button>
-            <button>
-              <img src={UnOrderedListIcon} alt="" />
-            </button>
-            <button>
-              <img src={JustifyTextIcon} alt="" />
-            </button>
-            <button>
-              <img src={RightTextIcon} alt="" />
-            </button>
-            <button>
-              <img src={CenterTextIcon} alt="" />
-            </button>
-            <button>
-              <img src={UnderlineIcon} alt="" />
-            </button>
-            <button>
-              <img src={ItalicIcon} alt="" />
-            </button>
-            <button>
-              <img src={BoldIcon} alt="" />
-            </button>
-          </div>
-          <form className="new-comment__form">
-            <textarea className="new-comment__textarea" placeholder="نظر خودتون رو در مورد این برنامه بنویسید ..." />
-            <div className="new-comment__button__container">
-              <button>ثبت نظر</button>
-            </div>
-          </form>
-        </div>
+
+      <div className="comments__content content">
         <div className="comments__container">
+          <CreateComment />
+
           <Comment />
         </div>
       </div>
--- src/pages/singleEvent.tsx
+++ src/pages/singleEvent.tsx
@@ -4,17 +4,15 @@ import AnimatedPage from "../components/animatedPage";
 import PathContainer from "../components/pathContainer";
 import Sidebar from "../components/singleEvent/sidebar";
 import Comments from "../components/singleEvent/comments";
-
+import EventInfo from "../components/singleEvent/eventInfo";
 import SingleEventEvents from "../components/singleEvent/singleEventEvents";
 
+import hamyarLogo from "../assets/mvp/hamyar-logo.png";
+import hamyarSpace from "../assets/mvp/hamyar-space.png";
 // Icons
 import InProgressIcon from "./../assets/icons/in-progress.svg";
-import CheckIcon from "./../assets/icons/check-outline.svg";
-import FillIcon from "./../assets/icons/fill.svg";
-import CapacityIcon from "./../assets/icons/capacity.svg";
 
 import "../styles/singleEvent.scss";
-import EventInfo from "../components/singleEvent/eventInfo";
 
 function Event() {
   const location = useLocation();
@@ -32,12 +30,27 @@ function Event() {
           <div className={"header__container"}>
             <img src={InProgressIcon} alt="" />
             <h1>
-              رویداد <span className={english.test(eventName[0]) ? "english" : ""}>{eventName}</span>
+              <span className={english.test(eventName[0]) ? "english" : ""}>{eventName}</span>
             </h1>
           </div>
 
           <div className="container__content">
-            <EventInfo />
+            <EventInfo
+              title={eventName}
+              supporter="همیار اکادمی"
+              date="14 مرداد ماه 1400"
+              time="16:00 بعداظهر"
+              meal="صبحانه کله پاچه"
+              cost={256000}
+              capacityNum={302}
+              filledNum={285}
+              link="https://www.google.com/"
+              locationLink="https://goo.gl/maps/TYNykj9YJx2pggqs6"
+              saved={false}
+              logo={hamyarLogo}
+              images={[hamyarSpace, hamyarSpace, hamyarSpace]}
+              dsrc="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد."
+            />
 
             <Sidebar />
           </div>
